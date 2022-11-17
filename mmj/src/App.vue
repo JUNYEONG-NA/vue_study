c077c246ee9ff6e62754913a2e978b88cb014cb2
jy-na <njy305@hkcloud.co.kr>
2022-11-17 11:47:08 +0900

    todo list 양방향 데이터 바인딩

--- mmj/src/App.vue
+++ mmj/src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center"> Todo app</h1>
-    <input v-model="todoText"
-    type="text"
-    class="w-100 p-2"
-    placeholder="Type todo"
-    @keyup.enter="addTodo">
+    <input 
+      v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo">
     <hr>
     <Todo
       v-for="todo in todos"
       :key="todo.id"
       :todo="todo"
-      @toggle-checkbox="toggleCheckbox"
-      @click-delete="deleteTodo"/>
+      @toggle-Checkbox="toggleCheckbox"
+      />
 
     
   </div>
@@ -24,8 +25,29 @@ import Todo from "@/components/ToDo.vue"
 export default {
   components:{
     Todo
+  },
+  data(){
+    return{
+      todoText:'',
+      todos:[
+        {id : 1, text: "buy a car", checked : false },
+        {id : 2, text: "buy a house", checked : true },
+        {id : 3, text: "buy a phone", checked : false }
+      ]
+    }
+  },
+  methods:{
+    addTodo(e){
+      this.todos.push({
+        id : this.id++,
+        text : e.target.value,
+        checked : false,
+        })
+        this.todoText=''
+    },
+    togglecheckbox({id, checked}){
+        console.log(id, checked)
+    }
   }
-
-  
 }
 </script>
--- mmj/src/components/ToDo.vue
+++ mmj/src/components/ToDo.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-  <input type="checkbox">
-    <span class="mt-3">hi</span>
+      {{todo.checked}}
+  <input type="checkbox" :checked="todo.checked"
+  @change="toggleCheckbox" >
+    <span class="mt-3">{{ todo.text }}</span>
   </div>
 </template>
 
 <script>
 export default {
-
+    props:{
+        todo : {
+            type : Object,
+            reqires : true
+        }
+    },
+    methods :{
+        toggleCheckbox(e){
+            this.$emit('toggle-Checkbox',{
+                id : this.todo.id,
+                checked : e.target.checked
+            })
+        }
+    }
 }
 </script>
 
